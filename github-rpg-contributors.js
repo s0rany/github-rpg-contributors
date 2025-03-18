7e7ffc987c1dcfb5a68eba008d396b5d631eccbf
s0rany <sfc5954@psu.edu>
2025-03-18 12:52:10 -0400

    Update github-rpg-contributors.js

--- github-rpg-contributors.js
+++ github-rpg-contributors.js
@@ -5,6 +5,7 @@
 import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
+import '@haxtheweb/rpg-character/rpg-character.js';
 
 /**
  * `github-rpg-contributors`
